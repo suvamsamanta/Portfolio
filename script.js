d147fb6350caf0e4cc6e530f19182bbaba8b8dad
suvamsamnta <suvamsamanta06@gmail.com>
2026-02-09 11:52:37 +0530

    Add Small Animation

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function DownloadFile() {
     downloadLink.download = 'download.png';
     downloadLink.click();
 }
+
+
+// Skill Animation
+const skillCards = document.querySelectorAll(".Skill-Card");
+
+const skillObserver = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                skillCards.forEach((card, i) => {
+                    setTimeout(() => {
+                        card.classList.add("show");
+                    }, i * 120);
+                });
+            }
+        });
+    },
+    { threshold: 0.3 }
+);
+
+skillObserver.observe(document.querySelector("#skill"));
